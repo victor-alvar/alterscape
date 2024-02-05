d73d061da72eb590e0d65c9fa7f0363aa9d88a2a
Victor Alvar <victor.alvar@tutanota.com>
2024-02-05 16:03:39 -0300

    Add initial files.

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title: "Alterscape your guide on all your trips",
+  description:
+    "Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ components/Footer.tsx
@@ -0,0 +1,33 @@
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-full flex-col gap-14">
+        <div className="border bg-gray-20" />
+        <p className="regular-14 w-full text-center text-gray-30">
+          © 2023 Alterscape | All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
+  );
+};
+
+export default Footer;
